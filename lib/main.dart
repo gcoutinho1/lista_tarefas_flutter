0958bca6925193fe595b3b8c6eff928d4fdfe4e9
Guilherme Coutinho <guilhermecoutinho7777@gmail.com>
2020-07-24 17:05:33 -0300

    UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+import 'dart:ffi';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
 //Created by Guilherme Coutinho @ 2020
 void main(){
   runApp(MaterialApp(
@@ -12,11 +17,68 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  List _todoList = [];
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Lista de Tarefas"),
+        backgroundColor: Colors.deepOrangeAccent,
+        centerTitle: true,
+      ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.fromLTRB(17, 1, 7, 1),
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                        labelText: "Nova Tarefa",
+                        labelStyle: TextStyle(color: Colors.deepOrangeAccent)
+                    ),
+                  ),
+                ),
+                RaisedButton(
+                  color: Colors.deepOrangeAccent,
+                  child: Text("ADD"),
+                  textColor: Colors.white,
+                  onPressed: () {},
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Future<File> _getFile() async {
+    final directory = await getApplicationDocumentsDirectory();
+    return File("${directory.path}/data.json");
+  }
+
+  Future<File> _saveData() async {
+    String data = json.encode(_todoList);
+    final file = await _getFile();
+    return file.writeAsString(data);
+
+  }
+
+  Future<String> _readData() async{
+    try {
+      final file = await _getFile();
+      return file.readAsString();
+    }catch (e){
+      return null;
+    }
   }
 }
+
+
 
 
 
