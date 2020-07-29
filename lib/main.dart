641d4f0d20f7c5fffa8006483a4b0968ee4c62c5
Guilherme Coutinho <guilhermecoutinho7777@gmail.com>
2020-07-29 15:48:34 -0300

    added the dismissible function to remove tasks

--- lib/main.dart
+++ lib/main.dart
@@ -78,27 +78,55 @@ class _HomeState extends State<Home> {
             child: ListView.builder(
                 padding: EdgeInsets.only(top: 10),
                 itemCount: _todoList.length,
-                itemBuilder: (context, index) {
-                  return CheckboxListTile(
-                    title: Text(_todoList[index]["title"]),
-                    value: _todoList[index]["ok"],
-                    secondary: CircleAvatar(
-                      child: Icon(
-                          _todoList[index]["ok"] ? Icons.check : Icons.error),
-                    ),
-                    onChanged: (c) {
-                      setState(() {
-                        _todoList[index]["ok"] = c;
-                        _saveData();
-                      });
-                    },
-                  );
-                }),
+                itemBuilder: buildItem),
           )
         ],
       ),
     );
   }
+
+  Widget buildItem(context, index) {
+    return Dismissible(
+      key: Key(DateTime.now().millisecondsSinceEpoch.toString()),
+      background: Container(
+        color: Colors.red,
+        child: Align(
+          alignment: Alignment(-0.9, 0),
+          child: Icon(Icons.delete, color: Colors.white,),
+        ),
+      ),
+      direction: DismissDirection.startToEnd,
+      child: CheckboxListTile(
+        title: Text(_todoList[index]["title"]),
+        value: _todoList[index]["ok"],
+        secondary: CircleAvatar(
+          child: Icon(
+              _todoList[index]["ok"] ? Icons.check : Icons.error),
+        ),
+        onChanged: (c) {
+          setState(() {
+            _todoList[index]["ok"] = c;
+            _saveData();
+          });
+        },
+      ),
+    );
+  }
+
+  /*CheckboxListTile(
+  title: Text(_todoList[index]["title"]),
+  value: _todoList[index]["ok"],
+  secondary: CircleAvatar(
+  child: Icon(
+  _todoList[index]["ok"] ? Icons.check : Icons.error),
+  ),
+  onChanged: (c) {
+  setState(() {
+  _todoList[index]["ok"] = c;
+  _saveData();
+  });
+  },
+  );*/
 
   Future<File> _getFile() async {
     final directory = await getApplicationDocumentsDirectory();
